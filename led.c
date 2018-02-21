50f7afba8f4a4126aad73a92092181b6c1e76220
cdavi014 <cdavi014@users.noreply.github.com>
2018-02-21 22:17:50 +0000

    Update led.c
    Version 2

--- led.c
+++ led.c
@@ -1,39 +1,56 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/mm.h>
 #include <asm/io.h>
-#include <linux/gpio.h>
-
-#define LED_PIN 	21			/* GPIO pin to toggle. */
-
-/* INIT */
-static int __init led_init(void) {
-  printk("hello led!\n");
-  
-  gpio_request(LED_PIN, "sysfs");
-  gpio_direction_output(LED_PIN, false);
-  gpio_export(LED_PIN, false);
-  gpio_set_value(LED_PIN, 1);
-
-  return 0;
-}
-
-/* SYSFS */
-// TODO
-
-/* CLEAN UP */
-static void __exit led_exit(void) {
-  printk(KERN_ALERT "bye led!\n");
-  
-  gpio_set_value(LED_PIN, 0);
-  gpio_unexport(LED_PIN);
-  gpio_free(LED_PIN);
-  // Get the correct GPSETn values and CLEARn
-}
+#include <linux/timer.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/mm.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Carlos R. Davila");
-MODULE_DESCRIPTION("Assingment 1 LED GPIO module driver.");
 
-module_init(led_init);
-module_exit(led_exit);
+struct GpioRegisters {
+    uint32_t GPFSEL[6];
+    uint32_t Reserved1;
+    uint32_t GPSET[2];
+    uint32_t Reserved2;
+    uint32_t GPCLR[2];
+};
+
+struct GpioRegisters *s_pGpioRegisters;
+
+static void SetGPIOFunction(int GPIO, int functionCode)
+{
+    int registerIndex = GPIO / 10;
+    int bit = (GPIO % 10) * 3;
+
+    unsigned oldValue = s_pGpioRegisters->GPFSEL[registerIndex];
+    unsigned mask = 0b111 << bit;
+    //printk("Changing function of GPIO%d from %x to %x\n", GPIO, (oldValue >> bit) & 0b111, functionCode);
+    s_pGpioRegisters->GPFSEL[registerIndex] = (oldValue & ~mask) | ((functionCode << bit) & mask);
+}
+
+static void SetGPIOOutputValue(int GPIO, bool outputValue)
+{
+    if (outputValue)
+        s_pGpioRegisters->GPSET[GPIO / 32] = (1 << (GPIO % 32));
+    else
+        s_pGpioRegisters->GPCLR[GPIO / 32] = (1 << (GPIO % 32));
+}
+
+static const int LedGpioPin = 21;
+#define PERIPH_BASE 0x20000000
+#define GPIO_BASE (PERIPH_BASE + 0x200000)
+
+static int __init LedBlinkModule_init(void) {
+    s_pGpioRegisters = (struct GpioRegisters *)ioremap(GPIO_BASE, sizeof(struct GpioRegisters));
+    SetGPIOFunction(LedGpioPin, 0b001);  	//Configure the pin as output
+    return 0;
+}
+
+static void __exit LedBlinkModule_exit(void) {
+    iounmap(s_pGpioRegisters);
+    SetGPIOFunction(LedGpioPin, 0);  	//Configure the pin as input
+}
+
+module_init(LedBlinkModule_init);
+module_exit(LedBlinkModule_exit);
