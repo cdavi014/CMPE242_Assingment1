05ae3c2588ee6f5d88a2330539983b15b6456b3d
cdavi014 <cdavi014@users.noreply.github.com>
2018-02-23 07:15:20 +0000

    Update led.c
    Updated address to 0X3F000000 according to BCM2837 documentation for physical base address of peripheral devices.
    Other bug fixes.

--- led.c
+++ led.c
@@ -1,56 +1,43 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <asm/io.h>
-#include <linux/timer.h>
-#include <linux/device.h>
-#include <linux/err.h>
-#include <linux/mm.h>
+/**
+ * Author: Carlos R. Davila
+ * SJSU ID: 010779067
+ * Original Code: https://sysprogs.com/VisualKernel/tutorials/raspberry/leddriver/
+ * Last Modified: Feb-22-2-18
+ */
+#include "led.h"
 
 MODULE_LICENSE("GPL");
-
-struct GpioRegisters {
-    uint32_t GPFSEL[6];
-    uint32_t Reserved1;
-    uint32_t GPSET[2];
-    uint32_t Reserved2;
-    uint32_t GPCLR[2];
-};
+MODULE_AUTHOR("Carlos R. Davila");
 
 struct GpioRegisters *s_pGpioRegisters;
 
-static void SetGPIOFunction(int GPIO, int functionCode)
-{
-    int registerIndex = GPIO / 10;
-    int bit = (GPIO % 10) * 3;
+static void SetGPIOFunction(int GPIO, int functionCode) {
+  int registerIndex = GPIO / 10;   // Select the correct GPFSEL to change
+  int bit = (GPIO % 10) * 3;	   // Set the bit offset of the correct GPIO
 
-    unsigned oldValue = s_pGpioRegisters->GPFSEL[registerIndex];
-    unsigned mask = 0b111 << bit;
-    //printk("Changing function of GPIO%d from %x to %x\n", GPIO, (oldValue >> bit) & 0b111, functionCode);
-    s_pGpioRegisters->GPFSEL[registerIndex] = (oldValue & ~mask) | ((functionCode << bit) & mask);
+  unsigned oldValue = s_pGpioRegisters->GPFSEL[registerIndex];
+  unsigned mask = 0b111 << bit;
+  s_pGpioRegisters->GPFSEL[registerIndex] = (oldValue & ~mask) | ((functionCode << bit) & mask);
 }
 
-static void SetGPIOOutputValue(int GPIO, bool outputValue)
-{
-    if (outputValue)
-        s_pGpioRegisters->GPSET[GPIO / 32] = (1 << (GPIO % 32));
-    else
-        s_pGpioRegisters->GPCLR[GPIO / 32] = (1 << (GPIO % 32));
+static void SetGPIOOutputValue(int GPIO, bool outputValue) {
+  if (outputValue)
+    s_pGpioRegisters->GPSET[GPIO / 32] = (1 << (GPIO % 32));
+  else
+    s_pGpioRegisters->GPCLR[GPIO / 32] = (1 << (GPIO % 32));
 }
 
-static const int LedGpioPin = 21;
-#define PERIPH_BASE 0x20000000
-#define GPIO_BASE (PERIPH_BASE + 0x200000)
-
-static int __init LedBlinkModule_init(void) {
-    s_pGpioRegisters = (struct GpioRegisters *)ioremap(GPIO_BASE, sizeof(struct GpioRegisters));
-    SetGPIOFunction(LedGpioPin, 0b001);  	//Configure the pin as output
-    return 0;
+static int __init led_init(void) {
+  s_pGpioRegisters = (struct GpioRegisters *)ioremap(GPIO_BASE, sizeof(struct GpioRegisters));
+  SetGPIOFunction(LedGpioPin, 0b001);  	//Configure the pin as output
+  SetGPIOOutputValue(LedGpioPin, true);
+  return 0;
 }
 
-static void __exit LedBlinkModule_exit(void) {
-    iounmap(s_pGpioRegisters);
-    SetGPIOFunction(LedGpioPin, 0);  	//Configure the pin as input
+static void __exit led_exit(void) {
+  SetGPIOFunction(LedGpioPin, 0);  		//Configure the pin as input
+  iounmap(s_pGpioRegisters);
 }
 
-module_init(LedBlinkModule_init);
-module_exit(LedBlinkModule_exit);
+module_init(led_init);
+module_exit(led_exit);
