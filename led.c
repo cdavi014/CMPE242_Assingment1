5be20d0fef4f01af2cf016c2401dd2317dc6f8bb
cdavi014 <cdavi014@users.noreply.github.com>
2018-02-23 22:23:43 -0800

    Update led.c
    Comment changes.

--- led.c
+++ led.c
@@ -2,7 +2,7 @@
  * Author: Carlos R. Davila
  * SJSU ID: 010779067
  * Original Code: https://sysprogs.com/VisualKernel/tutorials/raspberry/leddriver/
- * Last Modified: Feb-22-2-18
+ * Last Modified: Feb-23-2-18
  */
 #include "led.h"
 
@@ -12,8 +12,8 @@ MODULE_AUTHOR("Carlos R. Davila");
 struct GpioRegisters *s_pGpioRegisters;
 
 static void SetGPIOFunction(int GPIO, int functionCode) {
-  int registerIndex = GPIO / 10;   // Select the correct GPFSEL to change
-  int bit = (GPIO % 10) * 3;	   // Set the bit offset of the correct GPIO
+  int registerIndex = GPIO / 10;    // Select the correct GPFSEL to change.
+  int bit = (GPIO % 10) * 3;	      // Set the bit offset of the correct GPIO.
 
   unsigned oldValue = s_pGpioRegisters->GPFSEL[registerIndex];
   unsigned mask = 0b111 << bit;
@@ -22,21 +22,22 @@ static void SetGPIOFunction(int GPIO, int functionCode) {
 
 static void SetGPIOOutputValue(int GPIO, bool outputValue) {
   if (outputValue)
-    s_pGpioRegisters->GPSET[GPIO / 32] = (1 << (GPIO % 32));
+    s_pGpioRegisters->GPSET[GPIO / 32] = (1 << (GPIO % 32));  // Set pin to high.
   else
-    s_pGpioRegisters->GPCLR[GPIO / 32] = (1 << (GPIO % 32));
+    s_pGpioRegisters->GPCLR[GPIO / 32] = (1 << (GPIO % 32));  // Set pin to low.
 }
 
 static int __init led_init(void) {
-  s_pGpioRegisters = (struct GpioRegisters *)ioremap(GPIO_BASE, sizeof(struct GpioRegisters));
-  SetGPIOFunction(LedGpioPin, 0b001);  	//Configure the pin as output
-  SetGPIOOutputValue(LedGpioPin, true);
+  s_pGpioRegisters = (struct GpioRegisters *)ioremap( 
+    GPIO_BASE, sizeof(struct GpioRegisters));         // Map physical address to virtual address space.
+  SetGPIOFunction(LedGpioPin, 0b001);  	              // Configure the pin as output.
+  SetGPIOOutputValue(LedGpioPin, true);               // Turn LED on.
   return 0;
 }
 
 static void __exit led_exit(void) {
-  SetGPIOFunction(LedGpioPin, 0);  		//Configure the pin as input
-  iounmap(s_pGpioRegisters);
+  SetGPIOFunction(LedGpioPin, 0);  		                // Configure the pin as input.
+  iounmap(s_pGpioRegisters);                          // Unmap physical to virtual addresses.
 }
 
 module_init(led_init);
