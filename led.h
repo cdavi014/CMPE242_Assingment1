406543a89c34981a3f5ebd84e7b3ffb7d0462bcc
cdavi014 <cdavi014@users.noreply.github.com>
2018-02-23 22:19:49 -0800

    Update led.h
    Added comments.

--- led.h
+++ led.h
@@ -11,11 +11,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-#define PERIPH_BASE 0x3F000000
-#define GPIO_BASE (PERIPH_BASE + 0x200000)
+#define PERIPH_BASE 0x3F000000              // Base address of peripheral address range
+#define GPIO_BASE (PERIPH_BASE + 0x200000)  // Base address of GPIO pins
 
-static const int LedGpioPin = 21;
+static const int LedGpioPin = 21;           // Pin LED is connected to
 
+/**
+* Set a contiguous range of memory for the GPIO registers.
+* Space for 6 GPFSELn registers (0-5).
+* Space for reserved address 0x7E20 0018 (Bus Address) or 0x3F20 0018 (Physical Address).
+* Space for 2 GPSET registers.
+* Space for reserved address 0x7E20 0024 (Bus Address) or 0x3F20 0024 (Physical Address).
+* Space for 2 GPCLR registers.
+*/
 struct GpioRegisters {
   volatile uint32_t GPFSEL[6];
   volatile uint32_t Reserved1;
@@ -24,5 +32,16 @@ struct GpioRegisters {
   volatile uint32_t GPCLR[2];
 };
 
+/* 
+* Function to set the function of an individual pin. (input, output...etc).
+* int GPIO:         GPIO pin to set the funtion for.
+* int functionCode: The code to set for the GPIO pin.
+*/
 static void SetGPIOFunction(int GPIO, int functionCode);
+
+/* 
+* Function to set the output value of an individual pin. (0 or 1, low or high).
+* int GPIO:         GPIO pin to set the funtion for.
+* int outputValue:  The output to set the pin to.
+*/
 static void SetGPIOOutputValue(int GPIO, bool outputValue);
