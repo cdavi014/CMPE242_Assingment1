9e68e558e24fd3f105a845d6ea36bf1fddba5add
cdavi014 <cdavi014@users.noreply.github.com>
2018-02-28 09:02:42 +0000

    Update led.h
    Added support for sysfs

--- led.h
+++ led.h
@@ -9,11 +9,15 @@
 #include <linux/mm.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
 
 #define PERIPH_BASE 0x3F000000              // Base address of peripheral address range
 #define GPIO_BASE (PERIPH_BASE + 0x200000)  // Base address of GPIO pins
 
 static const int LedGpioPin = 21;           // Pin LED is connected to
+static int led_status = 0;					// 0-off, 1-on
+static struct kobject *led_kobj;
 
 /**
 * Set a contiguous range of memory for the GPIO registers.
@@ -31,16 +35,26 @@ struct GpioRegisters {
   volatile uint32_t GPCLR[2];
 };
 
+/*
+ * Function callback for sysfs interface that will take care of powering 
+ * on or off the LED
+ */
+static ssize_t led_show(struct kobject *kobj, 
+						struct kobj_attribute *attr, char *buf);
+static ssize_t led_store(struct kobject *kobj, 
+						 struct kobj_attribute *attr, 
+						 const char *buf, size_t count);
+
 /* 
-* Function to set the function of an individual pin. (input, output...etc).
-* int GPIO:         GPIO pin to set the funtion for.
-* int functionCode: The code to set for the GPIO pin.
-*/
+ * Function to set the function of an individual pin. (input, output...etc).
+ * int GPIO:         GPIO pin to set the funtion for.
+ * int functionCode: The code to set for the GPIO pin.
+ */
 static void SetGPIOFunction(int GPIO, int functionCode);
 
 /* 
-* Function to set the output value of an individual pin. (0 or 1, low or high).
-* int GPIO:         GPIO pin to set the funtion for.
-* int outputValue:  The output to set the pin to.
-*/
+ * Function to set the output value of an individual pin. (0 or 1, low or high).
+ * int GPIO:         GPIO pin to set the funtion for.
+ * int outputValue:  The output to set the pin to.
+ */
 static void SetGPIOOutputValue(int GPIO, bool outputValue);
